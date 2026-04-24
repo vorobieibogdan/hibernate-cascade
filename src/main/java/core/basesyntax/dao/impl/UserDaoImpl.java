d4a7eb5403b114667d9c8ec0a711ddfc111f8c0f
Bohdan Vorobiei <vbs@ukr.net>
2026-04-25 00:40:43 +0300

    The task was completed interestingly.6

--- src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
@@ -1,12 +1,12 @@
 package core.basesyntax.dao.impl;
 
-import core.basesyntax.HibernateUtil;
 import core.basesyntax.dao.UserDao;
 import core.basesyntax.model.User;
-import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import java.util.List;
+import java.util.Optional;
 
 public class UserDaoImpl implements UserDao {
     private final SessionFactory sessionFactory;
@@ -15,43 +15,62 @@ public class UserDaoImpl implements UserDao {
         this.sessionFactory = sessionFactory;
     }
 
-    public UserDaoImpl() {
-        this(HibernateUtil.getSessionFactory());
+    @Override
+    public User save(User user) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(user);
+            transaction.commit();
+            return user;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't save user", e);
+        }
+    }
+
+    @Override
+    public Optional<User> getById(Long id) {
+        try (Session session = sessionFactory.openSession()) {
+            User user = session.createQuery(
+                            "FROM User u LEFT JOIN FETCH u.comments WHERE u.id = :id",
+                            User.class)
+                    .setParameter("id", id)
+                    .uniqueResult();
+            return Optional.ofNullable(user);
+        }
     }
 
     @Override
     public User create(User entity) {
-        Session session = sessionFactory.openSession();
-        Transaction tx = session.beginTransaction();
-        session.persist(entity);
-        tx.commit();
-        session.close();
-        return entity;
+        return null;
     }
 
     @Override
     public User get(Long id) {
-        Session session = sessionFactory.openSession();
-        User user = session.get(User.class, id);
-        session.close();
-        return user;
+        return null;
     }
 
     @Override
     public List<User> getAll() {
-        Session session = sessionFactory.openSession();
-        List<User> users = session.createQuery("from User", User.class).getResultList();
-        session.close();
-        return users;
+        return null;
     }
 
     @Override
-    public void remove(User entity) {
-        Session session = sessionFactory.openSession();
-        Transaction tx = session.beginTransaction();
-        session.remove(entity);
-        tx.commit();
-        session.close();
+    public void remove(User user) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.remove(session.contains(user) ? user : session.merge(user));
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't remove user", e);
+        }
     }
 }
 
